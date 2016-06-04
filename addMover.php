7fc5446c871e461f053965632944628d3cc06435
rajan145 <rajanpatel007@ymail.com>
2016-06-03 22:28:34 -0400

    Update addMover.php

--- addMover.php
+++ addMover.php
@@ -14,20 +14,21 @@ include("DBConnection.php");
 	
 	//$conn = new DBConnection();
 	//$connectionObj=$conn->connection();
-	
+	//Movers Class 
 	class Movers{
+		//Mover Details 
 		private $m_Phone;
 		private $m_Fname; 
 		private $m_Lname; 
 		private $conn;
-		
+		//constructor to intilize variables
 		public function __construct($Phone, $Fname, $Lname) {
-        $this->m_Phone = $Phone;
-		$this->m_Fname = $Fname;
-		$this->m_Lname = $Lname;
-		$this->conn = new DBConnection();
+        		$this->m_Phone = $Phone;
+			$this->m_Fname = $Fname;
+			$this->m_Lname = $Lname;
+			$this->conn = new DBConnection();
 		}
-
+		//function which add new Mover into Database
 		function addMover($Phone, $Fname, $Lname){
 						
 			try{
@@ -36,6 +37,7 @@ include("DBConnection.php");
 			$nRows = $connectionObj->query("SELECT MoverPhone FROM movers where MoverPhone='$Phone'")->fetchColumn();
 			
 			if($nRows == NULL){
+			//database Query to insert into database table movers
 			$sql = "INSERT INTO movers (MoverPhone,FirstName,LastName) values('$Phone','$Fname','$Lname')";
 			$connectionObj->exec($sql);
 			echo "Mover added Successfully";
